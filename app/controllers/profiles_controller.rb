5ea834fdb2438d3344c55e6dba56aa8fcab94b76
slammyde7113 <dyers1@wit.edu>
2017-06-09 11:02:03 -0400

    Show Specific User Controller Added a specific user route and specific user controller method. Allows for queries on the Profiles table and returns all the entries of a single user instead of the whole table.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,6 @@
 class ProfilesController < ApplicationController
   before_action :set_profile, only: [:show, :update, :destroy]
-
+#  before_action :all_profiles, only: [:show_user]
   # GET /profiles
   def index
     @profiles = Profile.all
@@ -10,7 +10,18 @@ class ProfilesController < ApplicationController
 
   # GET /profiles/1
   def show
-    render json: @profile
+    render json: @profile.user
+  end
+
+  # GET /profiles/show-user/1
+  def show_user
+    @profiles = []
+    Profile.find_each do |profile|
+      if profile.user.id == params[:id].to_i
+        @profiles.push profile.user
+      end
+    end
+    render json: @profiles
   end
 
   # POST /profiles
@@ -43,6 +54,12 @@ class ProfilesController < ApplicationController
     def set_profile
       @profile = Profile.find(params[:id])
     end
+
+    # def all_profiles
+    #   Profile.where(params[:id]).each |x| do
+    #     @profiles_array.push(x)
+    #   end
+    # end
 
     # Only allow a trusted parameter "white list" through.
     def profile_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :profiles, except: [:new, :edit]
   resources :menu_items, except: [:new, :edit]
   resources :examples, except: [:new, :edit]
+  get 'profiles/show-user/:id' => 'profiles#show_user'
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
