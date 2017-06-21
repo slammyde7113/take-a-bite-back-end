dc59868b84ad7b0377ad4084d2c0d0f6a595297b
slammyde7113 <dyers1@wit.edu>
2017-06-21 09:48:32 -0400

    Added Current User To Auth Added current user to secure user profiles.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -80,7 +80,7 @@ class ProfilesController < OpenReadController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_profile
-      @profile = Profile.find(params[:id])
+      @profile = current_user.profiles.find(params[:id])
     end
 
 
